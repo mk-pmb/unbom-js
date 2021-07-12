0fc79015a90ca16a8040532b54be90213f200511
mk-pmb <mk-pmb@users.noreply.github.com>
2021-07-12 03:51:06 +0200

    v0.1.0: works but no tests

---
+++ unbom.js
@@ -0,0 +1,12 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+function unbom(x) {
+  if (!x) { return x; }
+  if (!x.slice) { return x; }
+  if (x.slice(0, 1) === '\uFEFF') { return x.slice(1); }
+  if (x.slice(0, 3).toString('utf8') === '\uFEFF') { return x.slice(3); }
+  return x;
+}
+
+module.exports = unbom;
